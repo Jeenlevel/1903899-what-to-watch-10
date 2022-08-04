ba1d4804a9664b624daaf4e032c9367376314c6c
Евгений <inlevel94@gmail.com>
2022-08-04 20:05:26 +0300

    Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmptyScreen from '../../pages/empty-screen/empty-screen';
+import MyList from '../../pages/my-list-screen/my-list-screen';
+import MovieScreen from '../../pages/movie-screen/movie-screen';
+import MovieReviewsScreen from '../../pages/movie-reviews-screen/movie-reviews-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
-// import Film from '../../types/film';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import PrivateRoute from '../private-route/private-route';
+import LoginScreen from '../../pages/login-screen/login-screen';
 
 type AppScreenProps = {
   title: string;
@@ -8,7 +16,39 @@ type AppScreenProps = {
 }
 
 function App({ title, genre, releaseDate }: AppScreenProps): JSX.Element {
-  return (<MainScreen title={title} genre={genre} releaseDate={releaseDate} />);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main}
+          element={<MainScreen title={title} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route path={AppRoute.SignIn}
+          element={<LoginScreen />}
+        />
+        <Route path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route path={AppRoute.Film}
+          element={<MovieScreen title={title} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route path={AppRoute.AddReview}
+          element={<MovieReviewsScreen title={title} genre={genre} releaseDate={releaseDate} />}
+        />
+        <Route path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route path="*"
+          element={<EmptyScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- project/src/types/film.ts
+++
@@ -1,9 +0,0 @@
-type Film = {
-  title: string,
-  posterSrc: string,
-  id?: string,
-  genre?: string,
-  year?: number,
-};
-
-export default Film;
